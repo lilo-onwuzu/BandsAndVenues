754ffbe8fd3205a2e2eaab2d25cad56dbc08c338
Lilo Onwuzu <lpr422@gmail.com>
2017-05-02 19:00:55 -0700

    update db info

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -5,5 +5,5 @@ public class DB {
   //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/band_tracker", null, null);
 
   // connect to heroku through maven dependency manager
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-243-107-66.compute-1.amazonaws.com:5432/dcp462jpuni78q","pyswayqmkuqbtm", "b340c5e463552f285bba4380680240aa8d9d1e8d23400bcdb85d3a4c47f10947");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-23-23-220-163.compute-1.amazonaws.com:5432/d4nussmo57b64n","opgatahnoziluu", "dd5f17208a6eb57363d9029c05294367268377ffe89a5279640469d98a647513");
 }
